8ddaf071280351ba26f6a1f0f7988c3ceaf169db
Massimiliano <massimiliano@maxict.it>
2019-11-05 17:23:17 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,11 +13,12 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    InAppBrowser
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppBrowser, InAppBrowserObject, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser) { }
+  browser: InAppBrowserObject;
+
+  message: string;
+
+  OnBrowserOpen(target) {
+    const opt: InAppBrowserOptions = {
+      hardwareback: 'no',
+      hidenavigationbuttons: 'yes',
+      hidespinner: 'yes',
+      hidden: 'yes',
+      location: 'no',
+      usewkwebview: 'no'
+
+    };
+
+    this.browser = this.iab.create('https://ilfotoalbum.maxict.it/browser.php', target, opt);
+
+    this.browser.on('message').subscribe(
+      mex => {
+        console.log(mex);
+        this.message = mex.message;
+      }
+    );
+
+
+    this.browser.on('loadstop').subscribe(event => {
+      this.browser.insertCSS({ code: 'body{color: red;' });
+      this.browser.executeScript(
+        {
+          code: '\
+      var message = \'this is the message\';\
+      var messageObj = {my_message: message};\
+      var stringifiedMessageObj = JSON.stringify(messageObj);\
+      webkit.messageHandlers.cordova_iab.postMessage(stringifiedMessageObj);'
+        }
+      );
+      this.browser.show();
+    });
+  }
 
 }
